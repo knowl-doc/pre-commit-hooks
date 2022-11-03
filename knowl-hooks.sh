35136d402bdabd69261ca9b10192728383bf6438
ashvin-sharma <sharma.ashvin@gmail.com>
2022-11-03 17:29:35 +0530

    Update knowl-hooks.sh

--- knowl-hooks.sh
+++ knowl-hooks.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd ../cli/
 echo "Hello World"
-python main.py review-linked-not-updated-snippets
-python main.py review-linked-updated-snippets
+python ../cli/main.py review-linked-not-updated-snippets
+python ../cli/main.py review-linked-updated-snippets

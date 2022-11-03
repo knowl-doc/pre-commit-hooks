02a6b703226d913d89c5b923d8aafa314471427a
ashvin-sharma <sharma.ashvin@gmail.com>
2022-11-03 17:17:47 +0530

    Update knowl-hooks.sh

--- knowl-hooks.sh
+++ knowl-hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd ../cli/
 echo "Hello World"
 python https://github.com/knowl-doc/pre-commit-hooks/main1.py review-linked-not-updated-snippets
 python https://github.com/knowl-doc/pre-commit-hooks/main1.py review-linked-updated-snippets

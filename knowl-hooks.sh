7b09205efe54013bed39ba414cda667971341ce7
ashvin-sharma <sharma.ashvin@gmail.com>
2022-11-17 23:28:28 +0530

    commit

--- knowl-hooks.sh
+++ knowl-hooks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "Knowl pre-commit hook Loading"
 BIN_PATH="$HOME"
 WORKING_DIR="$BIN_PATH/knowl_temp"
 export PATH=$PATH:$WORKING_DIR
 exec < /dev/tty
+echo "Knowl pre-commit hook Loading"
+
 
 verify_wget() {
     BIN_WGET=$(which wget) || {
